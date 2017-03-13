d975aab0fe85986e158e81ae8486502a5ef869c8
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-13 17:51:13 +0700

    WIP 4

--- myApp/src/App.js
+++ myApp/src/App.js
@@ -16,10 +16,9 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      x: 20,
-      y: -15,
-      xEnemy: 650,
-      yEnemy: 0,
+      xPlayer: 20,
+      yPlayer: -15,
+      obstacles: [],
       z: 0,
       vx: 1,
       vy: -1
@@ -27,39 +26,47 @@ class App extends Component {
     this.loop = this.loop.bind(this)
   }
 
-  loop (tick) {
-    this.setState({
-      xEnemy: this.state.xEnemy - (this.state.vx * tick * 2),
-      yEnemy: this.state.yEnemy + (this.state.vy * tick * 2)
-    })
-    if(this.state.yEnemy < -187 || this.state.yEnemy >= 155) {
-      this.setState({
-        vy: this.state.vy * -1
+  createObstacle () {
+    let obstaclesContainer = []
+    let initYPos = [-187, 100]
+    let obstacleXPos = Math.floor(Math.random() * 600 + 600)
+    let obstacleYPos = initYPos[Math.floor(Math.random() * 2)]
+    let obstacleHeight = Math.floor(Math.random() * 160 + 60)
+    setInterval(() => {
+      obstaclesContainer.push({
+        x: obstacleXPos,
+        y: obstacleYPos,
+        height: obstacleHeight
       })
-    }
+      this.setState({
+        obstacles: obstaclesContainer
+      })
+    }, 1500)
+  }
+
+  loop (tick) {
+    this.createObstacle()
+    // if(this.state.yObstacle < -187 || this.state.yObstacle >= 155) {
+    //   this.setState({
+    //     vy: this.state.vy * -1
+    //   })
+    // }
+    // TODO: shift() the obstacles
   }
 
   componentDidMount () {
-    setInterval(() => this.loop(1), 1)
+    setInterval(() => this.loop(1), 1000)
   }
 
   render () {
     const styles = {
-      box: {
+      boxPlayer: {
         width: 30,
         height: 30,
-        top: this.state.y,
-        left: this.state.x,
+        top: this.state.yPlayer,
+        left: this.state.xPlayer,
         position: 'absolute',
         backgroundColor: 'red'
-      },
-      box2: {
-        width: 30,
-        height: 30,
-        top: this.state.yEnemy,
-        left: this.state.xEnemy,
-        position: 'absolute',
-        backgroundColor: 'blue'
       },
       screenBackground: {
         flex: 1,
@@ -76,13 +83,33 @@ class App extends Component {
 
     return (
       <View>
-        <View style={styles.box}>
+        <View style={styles.boxPlayer}>
         </View>
-        <View style={styles.box2}>
-        </View>
+        {this.state.obstacles.length > 0
+          ?
+          <View
+            style={{
+              width: 30,
+              height: this.state.obstacles[0].height,
+              top: this.state.obstacles[0].y,
+              left: this.state.obstacles[0].x-600,
+              position: 'absolute',
+              backgroundColor: 'blue'
+            }}
+          >
+          </View>
+          : <View></View>
+        }
       </View>
     )
   }
 }
 
+/*
+this.state.obstacles.map((obstacle, index) => {
+  (
+
+  )
+})
+*/
 export default App
